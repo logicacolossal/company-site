77b9d1a5d9aea2ef836386f343a4189725042ebb
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2026-02-01 18:11:07 +0000

    fix: fixing form schema with zod update

--- src/pages/contact-us/form-schema.ts
+++ src/pages/contact-us/form-schema.ts
@@ -3,14 +3,8 @@ import { z } from "zod";
 export type ContactFormSchemaProps = z.infer<typeof contactFormSchema>;
 
 export const contactFormSchema = z.object({
-  name: z.string({ required_error: "required-field" }).min(1, "required-field"),
-  company: z
-    .string({ required_error: "required-field" })
-    .min(1, "required-field"),
-  email: z
-    .string({ required_error: "required-field" })
-    .email({ message: "email-field" }),
-  message: z
-    .string({ required_error: "required-field" })
-    .min(1, "required-field"),
+  name: z.string().min(1, "required-field"),
+  company: z.string().min(1, "required-field"),
+  email: z.email({ error: "email-field" }),
+  message: z.string().min(1, "required-field"),
 });
